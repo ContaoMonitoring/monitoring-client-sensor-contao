3aacf3a97bb2abf49d4321ac3b655a00c3f0c8e7
Cliff Parnitzky <github@cliff-parnitzky.de>
2017-11-15 23:10:07 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,35 @@
+[![Latest Version on Packagist](http://img.shields.io/packagist/v/contao-monitoring/monitoring-client-sensor-contao.svg?style=flat)](https://packagist.org/packages/contao-monitoring/monitoring-client-sensor-contao)
+[![Installations via composer per month](http://img.shields.io/packagist/dm/contao-monitoring/monitoring-client-sensor-contao.svg?style=flat)](https://packagist.org/packages/contao-monitoring/monitoring-client-sensor-contao)
+[![Installations via composer total](http://img.shields.io/packagist/dt/contao-monitoring/monitoring-client-sensor-contao.svg?style=flat)](https://packagist.org/packages/contao-monitoring/monitoring-client-sensor-contao)
+
+Contao Extension: MonitoringClientSensorContao
+==============================================
+
+Provides a sensor for the MonitoringClient, which reads the Contao data.
+
+
+Installation
+------------
+
+Install the extension via composer: [contao-monitoring/monitoring-client-sensor-contao](https://packagist.org/packages/contao-monitoring/monitoring-client-sensor-contao).
+
+If you prefer to install it manually, download the latest release here: https://github.com/ContaoMonitoring/monitoring-client-sensor-contao/releases
+
+
+Tracker
+-------
+
+https://github.com/ContaoMonitoring/monitoring-client-sensor-contao/issues
+
+
+Compatibility
+-------------
+
+- Contao version >= 3.5.0 ... <  3.6.0
+
+Dependency
+----------
+
+This extension is dependent on the following extensions:
+
+- [[contao-monitoring/monitoring-client]](https://packagist.org/packages/contao-monitoring/monitoring-client)
---
+++ system/modules/MonitoringClientSensorContao/classes/MonitoringClientSensorContao.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2017 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Cliff Parnitzky 2017-2017
+ * @author     Cliff Parnitzky
+ * @package    MonitoringClientSensorContao
+ * @license    LGPL
+ */
+
+/**
+ * Run in a custom namespace, so the class can be replaced
+ */
+namespace Monitoring;
+
+/**
+ * Class MonitoringClientSensorContao
+ *
+ * Special sensor for the MonitoringClient to read the Contao data.
+ * @copyright  Cliff Parnitzky 2017-2017
+ * @author     Cliff Parnitzky
+ * @package    Controller
+ */
+class MonitoringClientSensorContao extends \Backend
+{
+  /**
+   * Constructor
+   */
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Get the data from the client.
+   */
+  public function readData($arrData)
+  {
+    $arrData['contao.version'] = VERSION . '.' . BUILD;
+    $arrData['contao.maintenanceMode'] = \Config::get('maintenanceMode') ? 'true' : 'false';
+    
+    return $arrData;
+  }
+}
+
+?>
